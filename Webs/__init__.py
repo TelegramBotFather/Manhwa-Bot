7337ccd629039ad356fb1150a2a023ff25459bfe
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2026-03-18 11:16:20 +0545

    Create __init__.py

---
+++ Webs/__init__.py
@@ -0,0 +1,31 @@
+from .comick import ComickWebs
+#from .mangamob import MangaMobWebs
+#from .mangafire import MangaFireWebs
+
+from .asurascans import AsuraScansWebs
+#from .flamecomics import FlameComicsWebs
+#from .demonicscans import DemonicScansWebs
+from .manhuafast import ManhuaFastWebs
+from .weebcentral import WeebCentralWebs
+from .manhwaclan import ManhwaClanWebs
+from .templetoons import TempleToonsWebs
+from .manhuaplus import ManhuaplusWebs
+from .mgeko import MgekoWebs
+from .manhwa18 import Manhwa18Webs
+from .manga18fx import Manga18fxWebs
+
+web_data = {
+    #" Comick ": ComickWebs(),
+    #" MangaMob ": MangaMobWebs(),
+    " Asura Scans ": AsuraScansWebs(),
+    #" Flame Comics": FlameComicsWebs(),
+    #" Demonic Scans ": DemonicScansWebs(),
+    " Manhua Fast ": ManhuaFastWebs(),
+    " Weeb Central ": WeebCentralWebs(),
+    " ManhwaClan ": ManhwaClanWebs(),
+    " TempleToons ":TempleToonsWebs(),
+    " Manhuaplus ": ManhuaplusWebs(),
+    " Mgeko ": MgekoWebs(),
+    " Manga18fx ": Manga18fxWebs(),
+    " Manhwa18 ":  Manhwa18Webs(),
+}
